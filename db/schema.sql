c8b43078e26afda616a3a95adf33013bc0a6e512
GarrettA01 <gman.anderson2001@gmail.com>
2023-01-22 10:55:32 -0700

    Added department seeds

--- db/schema.sql
+++ db/schema.sql
@@ -5,7 +5,7 @@ USE department_db;
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
-)
+);
 
 CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -15,7 +15,7 @@ CREATE TABLE role (
     FOREIGN KEY department_id
     REFERENCES department(id)
     ON DELETE SET NULL
-)
+);
 
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
