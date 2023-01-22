815c2eabb6d7777241a60ff723548488816bbe84
GarrettA01 <gman.anderson2001@gmail.com>
2023-01-22 11:01:28 -0700

    Added role seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,3 +4,14 @@ VALUES
 ("Finance"),
 ("Legal"),
 ("Sales");
+
+INSERT INTO role(title, salary, department_id)
+VALUES
+("Head Engineer", 120000, 1),
+("Junior Engineer", 90000, 1),
+("Lead Accountant", 80000, 2),
+("Accountant", 60000, 2),
+("Managing Partner", 150000, 3),
+("Lawyer", 100000, 3),
+("Sales Lead", 60000, 4),
+("Salesman", 40000, 4); 
